d4ec16a79392f53cbe1361615cbbd3504691a964
laurencepobst <laurence.pobst@gmail.com>
2026-02-13 13:48:30 +0100

    Add TrainingProgram class with run method

--- TRAINING PROGRAM.py
+++ TRAINING PROGRAM.py
@@ -1,1 +1,9 @@
+TRAINING PROGRAM
 
+class TrainingProgram:
+    def run(self):
+        runner = self.create_runner()
+        goal = self.choose_goal()
+        generator = self.select_generator(runner)
+        plan = generator.generate_plan(runner, goal)
+        self.display_plan(plan)
