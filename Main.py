919e44d3ba5da76f907944c0ec290222f24e3f67
leboucqzelie <leboucqzelie@gmail.com>
2026-02-13 14:00:07 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,14 @@
 import README.md
+import RUNNER.py
+import TRAINING GOAL - RACE.py
+import WORKOUT.py 
+import TRAINING PLAN.py
+import TRAINING PROGRAM.py
+import PLAN GENERATOR.py
 
 
 
 
 
 
+git pull
