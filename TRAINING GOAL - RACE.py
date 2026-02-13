e42607ac5936e0166090645e5e11ed2957de6b9b
laurencepobst <laurence.pobst@gmail.com>
2026-02-13 13:47:12 +0100

    Add TrainingGoal class for race training

--- TRAINING GOAL - RACE.py
+++ TRAINING GOAL - RACE.py
@@ -1,1 +1,6 @@
+TRAINING GOAL - RACE
 
+class TrainingGoal:
+    def __init__(self, distance_km, duration_weeks):
+        self.distance_km = distance_km
+        self.duration_weeks = duration_weeks
