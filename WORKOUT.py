64ea738107fa00ea774386dce9dfa75b4703a17a
laurencepobst <laurence.pobst@gmail.com>
2026-02-13 13:47:34 +0100

    Add Workout class with initialization and string method
    Fix distance assignment and add string representation method.

--- WORKOUT.py
+++ WORKOUT.py
@@ -1,1 +1,11 @@
+WORKOUT
 
+class Workout:
+    def __init__(self, workout_type, distance, intensity):
+        self.workout_type = workout_type  # easy, tempo, long, interval
+        self.distance = distance
+        self.distance = pace
+        self.intensity = intensity
+
+def __str__(self):
+    return f"{self.workout_type.title()} run - {self.distance} miles at {self.pace} minutes per kilometer"
